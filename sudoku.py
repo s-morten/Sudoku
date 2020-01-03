9658c4fb58a428dda7edfaa7f4c1e85398bd6459
Morten Stehr <mortenstehr@hotmail.de>
2020-01-03 22:22:04 +0100

    Can solve Sudoku boards via Backtracking. Struggles to create own new Boards

--- sudoku.py
+++ sudoku.py
@@ -1,15 +1,21 @@
 import keyboard
+import random
+import time
 BOARDLENGTH = 9
-#
+STARTING_NUMBERS = 3
+
+board = [[0] * 9 for n in range(9)]
+solve_board = [[0] * 9 for n in range(9)]
+
 board = [[5,3,0,0,7,0,0,0,0],
-           [6,0,0,1,9,5,0,0,0],
-           [0,9,8,0,0,0,0,6,0],
-           [8,0,0,0,6,0,0,0,3],
-           [4,0,0,8,0,3,0,0,1],
-           [7,0,0,0,2,0,0,0,6],
-           [0,6,0,0,0,0,2,8,0],
-           [0,0,0,4,1,9,0,0,5],
-           [0,0,0,0,8,0,0,7,9]]
+          [6,0,0,1,9,5,0,0,0],
+          [0,9,8,0,0,0,0,6,0],
+          [8,0,0,0,6,0,0,0,3],
+          [4,0,0,8,0,3,0,0,1],
+          [7,0,0,0,2,0,0,0,6],
+          [0,6,0,0,0,0,2,8,0],
+          [0,0,0,4,1,9,0,0,5],
+          [0,0,0,0,8,0,0,7,9]]
 
 #board = [[3,4,0,8,2,6,0,7,1],
 #           [0,0,8,0,0,0,9,0,0],
@@ -22,7 +28,61 @@ board = [[5,3,0,0,7,0,0,0,0],
 #           [4,1,0,3,8,9,0,6,2]]
 given = [[False] * 9 for n in range(9)]
 
+def copy_to_solve_board():
+    for x in range(0,9):
+        for y in range(0,9):
+            board[x][y] = solve_board[x][y]
 
+def copy_board():
+    for x in range(0,9):
+        for y in range(0,9):
+            full_board[x][y] = board[x][y]
+            solve_board[x][y] = board[x][y]
+
+def generate_full_board():
+    try_count = 0
+    for x in range(0,9):
+        for y in range(0,9):
+            while(True):
+                if(try_count >= 50):
+                    return False
+                number = random.randint(1,9)
+                try_count = try_count + 1
+                if(keyboard.is_pressed('q')):
+                    printBoard()
+                    print("------------------------------------------------------------------------")
+                    break
+                print(f"random number is {number} at {x},{y} ------ Try: {try_count}")
+                #time.sleep(0.5)
+                board[x][y] = number
+                if(checkRules(x, y)):
+                    try_count = 0
+                    break
+
+            if(keyboard.is_pressed('q')):
+                break
+        if(keyboard.is_pressed('q')):
+            break
+
+    printBoard()
+    return True
+
+###################################################
+def remove_numbers(numbers):
+    removed = 0
+    while(removed < numbers):
+        row = random.randint(0,8)
+        col = random.randint(0,8)
+        if(not board[row][col] == 0):
+            copy_to_solve_board()
+            backup = board[row][col]
+            board[row][col] = 0
+            if(solve(0,0)):
+                solve_board[row][col] = 0
+                removed = removed + 1
+            else:
+                board[row][col] = backup
+###################################################
 def printBoard():
     for x in range(0,9):
         for y in range(0,9):
@@ -124,9 +184,6 @@ def solve(a, b):
     yIdx = b
     ruleBreak = False
     while(xIdx < 9 and yIdx < 9):
-        if keyboard.is_pressed('q'):
-            printBoard()
-            break;
         if(not given[xIdx][yIdx]):
             # do while in python
             while(True):
@@ -137,23 +194,31 @@ def solve(a, b):
                     if(xIdx > 0):
                         #print("go one back x")
                         xIdx = xIdx - 1
+                        if(xIdx < 0):
+                            return False
                         while(given[xIdx][yIdx]):
                             if(not xIdx == 0):
                                 xIdx = xIdx - 1
                             elif(not yIdx == 0):
                                 xIdx = 8
                                 yIdx = yIdx - 1
+                            if(xIdx < 0 or yIdx < 0):
+                                return False
                         break
                     else:
                         #print("go one back y")
                         xIdx = 8
                         yIdx = yIdx-1
+                        if(xIdx < 0 or yIdx < 0):
+                            return False
                         while(given[xIdx][yIdx]):
                             if(not xIdx == 0):
                                 xIdx = xIdx - 1
                             elif(not yIdx == 0):
                                 xIdx = 8
                                 yIdx = yIdx - 1
+                            if(xIdx < 0 or yIdx < 0):
+                                return False
                         break
                 if(checkRules(xIdx,yIdx)):
                     ruleBreak = True
@@ -172,20 +237,33 @@ def solve(a, b):
                 xIdx = 0
                 yIdx = yIdx + 1
 
-for x in range(0,9):
-    for y in range(0,9):
-        print(board[x][y], end =" ")
-        if(board[x][y] != 0):
-            given[x][y] = True
-        else:
-            given[x][y] = False
-    print("\n")
+    return True
+def main():
+    #generate_full_board()
+    for x in range(0,9):
+        for y in range(0,9):
+            print(board[x][y], end =" ")
+            if(board[x][y] != 0):
+                given[x][y] = True
+            else:
+                given[x][y] = False
+        print("\n")
 
-solve(0,0)
+    solve(0,0)
 
-print("\n\n--------------------------------------------------------------------------------\n\n")
+    print("\n\n--------------------------------------------------------------------------------\n\n")
 
-for x in range(0,9):
-    for y in range(0,9):
-        print(board[x][y], end =" ")
-    print("\n")
+    for x in range(0,9):
+        for y in range(0,9):
+            print(board[x][y], end =" ")
+        print("\n")
+
+if __name__ == "__main__":
+    main()
+    # need_to_generate = True
+    # while(need_to_generate):
+    #     board = [[0] * 9 for n in range(9)]
+    #     need_to_generate = not generate_full_board()
+    # full_board = [[0] * 9 for n in range(9)]
+    # copy_board()
+    # remove_numbers(3)
